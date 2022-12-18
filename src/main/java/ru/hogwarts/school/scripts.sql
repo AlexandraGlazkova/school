a047c74fa119be078da0a8588df60a51a9852b2a
GlakovaA <22109@bk.ru>
2022-12-18 20:46:31 +0300

    домашка 4.2

--- src/main/java/ru/hogwarts/school/scripts.sql
+++ src/main/java/ru/hogwarts/school/scripts.sql
@@ -1,20 +1,20 @@
 
 -- 1. Получить всех студентов, возраст которых находится между 10 и 20
 -- (можно подставить любые числа, главное, чтобы нижняя граница была меньше верхней).
-SELECT * FROM students WHERE age BETWEEN 20 AND 23;
+SELECT * FROM student WHERE age BETWEEN 20 AND 23;
 -- 2. Получить всех студентов, но отобразить только список их имен.
-SELECT name FROM students;
+SELECT name FROM student;
 -- 3. Получить всех студентов, у которых в имени присутствует буква «О» (или любая другая).
-SELECT * FROM students WHERE name LIKE '%л%';
+SELECT * FROM student WHERE name LIKE '%л%';
 -- 4. Получить всех студентов, у которых возраст меньше идентификатора.
-SELECT * FROM students WHERE age<id;
+SELECT * FROM student WHERE age<id;
 -- 5. Получить всех студентов упорядоченных по возрасту.
-SELECT * FROM students ORDER BY age;
+SELECT * FROM student ORDER BY age;
 -- получить факультет студента
-SELECT f.id, s.name FROM students as s,faculty as f
+SELECT f.id, s.name FROM student as s,faculty as f
 WHERE s.faculty_id = f.id
   AND s.id = 4;
 -- получить студентов факультета
-SELECT f.id, s.name FROM students as s,faculty as f
+SELECT f.id, s.name FROM student as s,faculty as f
 WHERE s.faculty_id = f.id
   AND f.id = 3;
