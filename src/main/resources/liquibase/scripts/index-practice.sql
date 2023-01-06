e6029476464bdf44375398d2dac1212ef4cd3e57
GlakovaA <22109@bk.ru>
2023-01-06 17:31:34 +0300

    домашка 4.3

---
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -0,0 +1,4 @@
+-- Индекс для поиска по имени студента
+CREATE INDEX student_name_index ON student (name);
+-- Индекс для поиска по названию и цвету факультета
+CREATE INDEX faculty_name_color_index ON faculty (name, color);
